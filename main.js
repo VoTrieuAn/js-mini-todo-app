b93894cfb4146af120ad7be973c4418b5b354f82
VoTrieuAn <voan2068@gmail.com>
2025-09-04 15:15:11 +0700

    4. Thêm tính năng "Chỉnh sửa task"

--- main.js
+++ main.js
@@ -8,12 +8,27 @@ const taskList = document.querySelector("#task-list");
 const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector("#todo-input");
 
+taskList.onclick = function (e) {
+  const taskItem = e.target.closest(".task-item");
+  const taskIndex = taskItem.getAttribute("task-index");
+  const task = tasks[taskIndex];
+  if (e.target.closest(".edit")) {
+    const newTitle = prompt("Enter new task title:", task.title);
+    task.title = newTitle;
+    renderTasks();
+  } else if (e.target.closest(".done")) {
+    console.log("Done");
+  } else if (e.target.closest(".delete")) {
+    console.log("Delete");
+  }
+};
+
 todoForm.onsubmit = function (e) {
   e.preventDefault();
   const value = todoInput.value.trim();
   console.log(value);
   if (!value) {
-    return alert("Vui lòng nhập task");
+    return alert("Please enter new task");
   }
 
   tasks.push({
@@ -28,8 +43,10 @@ todoForm.onsubmit = function (e) {
 
 function renderTasks() {
   const html = tasks
-    .map((task) => {
-      return `<li class="task-item ${task.completed ? "completed" : ""}">
+    .map((task, index) => {
+      return `<li task-index=${index} class="task-item ${
+        task.completed ? "completed" : ""
+      }">
     <span class="task-title">${task.title}</span>
     <div class="task-action">
       <button class="task-btn edit">Edit</button>
