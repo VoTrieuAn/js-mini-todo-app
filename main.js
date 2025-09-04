eeb6467b09e2bea1270a17e84a05ba4e0dbf3f9b
VoTrieuAn <voan2068@gmail.com>
2025-09-04 14:42:29 +0700

    2. Render danh sách từ mảng "tasks"

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ const tasks = [
 ];
 
 const taskList = document.querySelector("#task-list");
+
+function renderTasks() {
+  const html = tasks
+    .map((task) => {
+      return `<li class="task-item ${task.completed ? "completed" : ""}">
+    <span class="task-title">${task.title}</span>
+    <div class="task-action">
+      <button class="task-btn edit">Edit</button>
+      <button class="task-btn done">${
+        task.completed ? "Mark as undone" : "Mark as done"
+      }</button>
+      <button class="task-btn delete">Delete</button>
+    </div>
+  </li>`;
+    })
+    .join("");
+
+  taskList.innerHTML = html;
+}
+
+renderTasks();
