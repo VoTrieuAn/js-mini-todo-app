288bc030c78c8acf6fe624ad786de580040ee3f5
VoTrieuAn <voan2068@gmail.com>
2025-09-04 14:37:01 +0700

    1. Khởi tạo dự án

---
+++ main.js
@@ -0,0 +1,7 @@
+const tasks = [
+  { title: "Design a website", completed: true },
+  { title: "Learn JavaScript", completed: false },
+  { title: "Build a Todo App", completed: true },
+];
+
+const taskList = document.querySelector("#task-list");
